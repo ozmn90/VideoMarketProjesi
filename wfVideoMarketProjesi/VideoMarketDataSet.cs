cb800429ca05aa22d703de15c28b6b8404902de2
Osman Yılmaz <ozmn90@gmail.com>
2016-07-19 00:38:15 +0300

    VideoMarket App
    Anasayfa , Filmler(Ürün İşlemleri) , Film Satış ,Film Sorgulama , Film
    Türleri , Müşteri Kaydı ,Müşteri Sorgulama , Raporlama Ve Satış
    Sorgulama sayfalarından olusan bir windowsform projesidir.Ado.Net bu
    projede yoğun olarak kullanılmıştır.
    
    Back up : VideoMarket.bak

---
+++ wfVideoMarketProjesi/VideoMarketDataSet.cs
@@ -0,0 +1,13 @@
+﻿namespace wfVideoMarketPRojesi {
+    
+    
+    public partial class VideoMarketDataSet {
+    }
+}
+
+namespace wfVideoMarketPRojesi.VideoMarketDataSetTableAdapters {
+    
+    
+    public partial class vw_DetayliSatisTableAdapter {
+    }
+}
